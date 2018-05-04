256980c3d1d3656bff982e3fe31b82b7e27458c8
Tim <wl52056@163.com>
2018-05-04 19:21:35 +0800

    dfd

--- src/main/java/com/black/wl520/controller/UserController.java
+++ src/main/java/com/black/wl520/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.black.wl520.controller;
 
 import com.black.wl520.bean.JSONResult;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    public Logger logger =  Logger.getLogger("UerController");
 
     @RequestMapping(value = "/userLogin", method = RequestMethod.GET,produces = "application/json")
     @ResponseBody
     public JSONResult userLogin() {
         JSONResult result = new JSONResult();
+        logger.error("asdfasdff");
         result.setError_code("0");
         result.setError_message("操作成功");
         return result;
